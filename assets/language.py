5264fce286ba032beb4287e55bb4e96a622e439f
matt <matthew.f.prado@valkyriepcs.com>
2020-10-25 00:20:06 -0700

    Update language.py

--- assets/language.py
+++ assets/language.py
@@ -1,2 +1,2 @@
-message = 'This is purely for aesthetic purposes; my repository has a language after putting this little piece of code.'
+message = 'This is purely for aesthetic purposes; my repository will be marked as having a language after putting this little piece of code.'
 print(message)
