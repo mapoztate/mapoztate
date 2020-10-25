9a7edef68a2699ee00ceeaa14b4627a44d89ffaf
matt <matthew.f.prado@valkyriepcs.com>
2020-10-25 08:39:37 -0700

    Changed language

---
+++ assets/language.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This is purely for aesthetic purposes; my repository will be marked as having a language after putting this little piece of code.";
+    return 0;
+}
--- assets/language.py
+++
@@ -1,2 +0,0 @@
-message = 'This is purely for aesthetic purposes; my repository will be marked as having a language after putting this little piece of code.'
-print(message)
