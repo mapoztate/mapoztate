fe8963eef304626e02b03bdddde1eee244ff7a1b
matt <matthew.f.prado@valkyriepcs.com>
2021-06-02 15:53:52 -0700

    Update language.cc

--- assets/language.cc
+++ assets/language.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "This is purely for aesthetic purposes; my repository will be marked as having a language after putting this little piece of code.";
+    std::cout << " ";
     return 0;
 }
